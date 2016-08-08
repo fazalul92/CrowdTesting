0865e066eb40dbd7791fe54aab0225a18d4ae727
fazalul92 <fazalul92@gmail.com>
2016-08-08 16:12:09 -0400

    Corrected Sequence in statuses (MySQL)

--- src/main/resources/db_data.sql
+++ src/main/resources/db_data.sql
@@ -449,16 +449,15 @@ INSERT INTO `presurvey_questions` (`id`, `description`, `question_type`, `answer
 -- Dumping data for table `statuses`
 --
 
-INSERT INTO `statuses` (`id`, `seq_no`, `title`, `filename`, `next_seq`) VALUES
-(1, 1, 'Pre-Survey', 'presurvey.jsp', 2),
-(2, 2, 'Personality Survey', 'persona.jsp', 3),
-(4, 5, 'Main Instructions', 'debrief.jsp', 6),
-(5, 5, 'Add Requirements', 'addUseCase.jsp', 6),
-(6, 6, 'Post-Survey', 'postsurvey.jsp', 7),
-(7, 7, 'Completion', 'completed.jsp', 8),
-(8, 5, 'View Requirements', 'requirements.jsp', 6),
-(9, 4, 'Group Creation', 'exec/groupassign.jsp', 5),
-(10, 3, 'Personality Survey', 'mbtipersona.jsp', 4);
+INSERT INTO `statuses` (`seq_no`, `title`, `filename`, `next_seq`) VALUES
+(1, 'Presurvey 1', 'presurvey.jsp', 2),
+(2, 'PreSurvey 2', 'persona.jsp', 3),
+(3, 'PreSurvey 3', 'mbtipersona.jsp', 4),
+(4, 'Group Creation', 'exec/groupassign.jsp', 5),
+(5, 'Main Instructions', 'debrief.jsp', 6),
+(5, 'View Requirements', 'requirements.jsp', 6),
+(6, 'Post-Survey', 'postsurvey.jsp', 7),
+(7, 'Completion', 'completed.jsp', 8);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
